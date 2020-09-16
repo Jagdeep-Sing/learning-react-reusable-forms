48b93d2498594e906a0ff96d7098f898b4163391
jagdeep.singh <jagdeep.singh@vtnetzwelt.com>
2020-09-16 10:19:48 +0530

    reference added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import InputField from './Components/InputField'
 
 function App() {
+  const inputRefs = React.useRef(
+    [
+      React.createRef(),
+      React.createRef()
+    ]
+  );
   const [data, setData] = React.useState({});
 
   const handleChange = (name, value) => {
     setData(prev => ({...prev, [name]: value}))
   }
-  console.log('t', data)
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    let isValid = true;
+
+    for (let i = 0; i < inputRefs.current.length; i++) {
+      const valid = inputRefs.current[i].current.validate();
+      console.log(valid)
+    }
+  }
   return (
     <div className="App">
-      <form>
+      <form onSubmit={submitForm}>
         <InputField
+          ref={inputRefs.current[0]}
           name ="username"
           label = "Username*:"
           onChange = {handleChange}
         />
 
         <InputField
+          ref={inputRefs.current[1]}
           name ="password"
           label = "Password*:"
           onChange = {handleChange}
         />
+        <button type="submit">SignUp</button>
       </form>
     </div>
   );
--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, {forwardRef, useImperativeHandle} from "react";
 
-const InputField = (props) => {
-    const [value, setValue] = React.useState("");
+const InputField = forwardRef(
+    (props, ref) => {
+        const [value, setValue] = React.useState("");
+    
+        const handleChange = (event) => {
+            setValue(event.target.value)
+            props.onChange(event.target.name, event.target.value)
+        }
 
-    const handleChange = (event) => {
-        setValue(event.target.value)
-        props.onChange(event.target.name, event.target.value)
+        const validate = () => {
+            return true;
+        }
+
+        useImperativeHandle(ref, () => {
+            return {
+                validate: () => validate()
+            }
+        })
+    
+        return(
+            <div className="input-wrapper">
+                {props.label && (
+                    <label>{props.label}</label>
+                )}
+            <input
+            placeholder={props.placeholder}
+            name={props.name}
+            onChange={(event) => handleChange(event)}
+            type={props.type}
+            value={props.value ? props.value : value}
+            autoComplete={props.autoComplete}
+            />
+        </div>
+        )
     }
-
-    return(
-        <div className="input-wrapper">
-            {props.label && (
-                <label>{props.label}</label>
-            )}
-        <input
-        placeholder={props.placeholder}
-        name={props.name}
-        onChange={(event) => handleChange(event)}
-        type={props.type}
-        value={props.value ? props.value : value}
-        autoComplete={props.autoComplete}
-        />
-    </div>
-    )
-}
+)
 
 InputField.defaultProps = {
     placeholder: "",
